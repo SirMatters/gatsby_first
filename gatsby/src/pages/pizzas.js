ead2319b12145eef5e4f7b265b6151c1241b7015
SirMatters <perov.krll@gmail.com>
2020-10-15 23:04:33 +0300

    Gatsby queries & single pizza display
    /pages/pizzas.js
    - create a query to get all the pizzas
    - the exported query is passed to the related PAGE component as a prop by Gatsby itself
    - pass the pizzas to <PizzaList/>
    
    /components/PizzaList.js
    - create <PizzaList/> component which gets pizzas from pizza page
    - create embeded component <SinglePizza/> and pass pizzas one by one in it

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,5 +1,37 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-const Pizzas = () => <p>Pizzas page</p>;
+import PizzaList from '../components/PizzaList';
+
+const Pizzas = ({ data }) => (
+  <>
+    <PizzaList pizzas={data.pizzas.nodes} />
+  </>
+);
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Pizzas;
